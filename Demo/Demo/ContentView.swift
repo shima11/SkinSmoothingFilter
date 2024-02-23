1cc9a04591075afb8970a115e1892247ba7a033f
shima11 <shima.jin@icloud.com>
2024-02-24 00:01:09 +0900

    update

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -6,8 +6,11 @@ import PhotosUI
 @available(iOS 16.0, *)
 struct ContentView: View {
 
-  @State private var selectedPhotoItem: PhotosPickerItem? = nil
-  @State var originalImage: Image? = nil
+  @State var selectedPhotoItem: PhotosPickerItem? = nil
+  @State var originalImage: UIImage? = nil
+  @State var inputAmount: Double = 0.5
+  @State var inputSharpness: Double = 0.3
+  @State var inputRadius: Double = 8.0
   @State var edittedImage: Image? = nil
 
   var body: some View {
@@ -15,34 +18,42 @@ struct ContentView: View {
       ScrollView {
         VStack {
 
-          if let originalImage {
-            originalImage
+          if let originalImage = originalImage {
+            Image(uiImage: originalImage)
               .resizable()
               .scaledToFit()
           }
 
-          if let edittedImage {
+          if let edittedImage = edittedImage {
             edittedImage
               .resizable()
               .scaledToFit()
           }
 
+          VStack {
+            Text("amount: \(inputAmount)")
+            Slider(value: $inputAmount, in: 0...1, step: 0.1)
+            Text("sharpness: \(inputSharpness)")
+            Slider(value: $inputSharpness, in: 0...1, step: 0.1)
+            Text("radius: \(inputRadius)")
+            Slider(value: $inputRadius, in: 0...40, step: 1)
+          }
+          .padding(.top, 24)
+          .padding(.horizontal, 24)
+
         }
+        .onChange(of: originalImage, perform: { _ in
+          perform()
+        })
+        .onChange(of: [inputAmount, inputSharpness, inputRadius], perform: { _ in
+          perform()
+        })
         .onChange(of: selectedPhotoItem) { selectedPhotoItem in
           Task {
             do {
               if let data = try await selectedPhotoItem?.loadTransferable(type: Data.self) {
                 if let uiImage = UIImage(data: data) {
-                  originalImage = .init(uiImage: uiImage)
-
-                  let ciImage = CIImage(image: uiImage)!
-
-                  let filter = SkinSmoothingFilter()
-                  filter.inputImage = ciImage
-
-                  if let result = filter.outputImage?.toUIImage() {
-                    edittedImage = .init(uiImage: result)
-                  }
+                  originalImage = uiImage
                 }
               }
             } catch {
@@ -62,6 +73,22 @@ struct ContentView: View {
       }
     }
   }
+
+  private func perform() {
+    guard let uiImage = originalImage else { return }
+    let ciImage = CIImage(image: uiImage)!
+
+    let filter = SkinSmoothingFilter()
+    filter.inputImage = ciImage
+    filter.inputAmount = inputAmount
+    filter.inputSharpness = inputSharpness
+    filter.inputRadius = inputRadius
+
+    if let result = filter.outputImage?.toUIImage() {
+      edittedImage = .init(uiImage: result)
+    }
+  }
+
 }
 
 extension CIImage {
